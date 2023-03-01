807adec5a9bdca698cb4bc0dc27028912040cf27
moeni <moeni@MSI>
2023-03-01 11:12:22 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Passport and floss!";
+            Console.WriteLine(message);
+
+
+            /*Console.WriteLine("Once upon a time \n" +"I was falling in love \n" + "Now I’m only falling apart" );
+            Console.WriteLine();
+            Console.WriteLine();*/
+        }
+    }
+}
