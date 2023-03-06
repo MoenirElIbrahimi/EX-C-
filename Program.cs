6d0b3a3119ee999fb5865dbd9b6c5b1de9b1f373
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 11:23:00 +0100

    1-13 EX

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            string message = "Passport and a toothbrush!";
+            int days = 200;
+            double hours = 3.5;
+            string code = "It is!";
+            Console.WriteLine("Days to summer:\n" +
+                days + "\nHours to lunch:\n" + hours + "\nCoding is fun:\n" + code); string message = "Passport and a toothbrush!";
             Console.WriteLine(message);
 
 
