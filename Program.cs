0ca8eae50297d4919192b684929324f0e9f67ad6
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 12:33:40 +0100

    1-18 EX

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@
         {
             Console.WriteLine("Give the first number!");
             string userInput = Console.ReadLine();
-            int first = Convert.ToInt32(userInput);
+            int first = Convert.ToInt32(userInput); // ask for 1st int ask for input and convert
+
             Console.WriteLine("Give the second number!");
             string userIn = Console.ReadLine();
-            int second = Convert.ToInt32(userIn);
-            int sum = first + second;
-            Console.WriteLine("the sum is "+ sum);
+            int second = Convert.ToInt32(userIn);// ask for 2nd int ask for input and convert
+
+            Console.WriteLine("Give the second number!");
+            string user = Console.ReadLine();
+            int third = Convert.ToInt32(user);// ask for 3rd int ask for input and convert
+
+            int sum = first + second + third;
+            Console.WriteLine("the sum is "+ sum); // calulates the 3 inputs by + and print it
 
 
         }
