331fab77c91c9535c569c131b607ebdff4352c83
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 13:45:52 +0100

    1-31 EX

--- Program.cs
+++ Program.cs
@@ -4,43 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give your percent [0-100]");
+            Console.WriteLine("Give a number:");
             string userInput = Console.ReadLine();
             int numberst = Convert.ToInt32(userInput);// ask 1st for input and convert it
 
 
-            if (numberst < 0) 
+            if (numberst % 2 == 0) 
             {
-            Console.WriteLine("Impossible");
+            Console.WriteLine("it is even.");
             }
-            else if (numberst > 100) 
-            { 
-            Console.WriteLine("Outstanding!");
-            }
-            else if (numberst > 89)
-            {
-            Console.WriteLine("5");
-            }
-            else if (numberst > 79)
-            {
-            Console.WriteLine("4");
-            }
-            else if (numberst > 69)
-            {
-            Console.WriteLine("3");
-            }
-            else if (numberst > 59)
-            {
-            Console.WriteLine("2");
-            }
-            else if (numberst > 49)
-            {
-            Console.WriteLine("1");
-            }
-            else if (numberst >= 0)
-            {
-            Console.WriteLine("Fail");
-            }
+            else { Console.WriteLine("It is odd."); }
         }
     }
 }
