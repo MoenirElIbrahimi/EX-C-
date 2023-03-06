adb48463064316d8d0b310c93d3da74ab9b70e42
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 14:10:35 +0100

    1-36 EX

--- Program.cs
+++ Program.cs
@@ -6,18 +6,28 @@
         {
 
 
-//string srtInput = Console.ReadLine();
+         //string srtInput = Console.ReadLine();
+         
+
             while (true)
             {
             Console.WriteLine("Give a number:");
             string userInput = Console.ReadLine();
             int number = Convert.ToInt32(userInput);// ask 1st for input and convert it
 
-                if (number == 42)
+                if (number > 0)
+                {
+                    int sum = number * number;
+                    Console.WriteLine(sum);
+                }
+                if (number < 0)
+                {
+                    Console.WriteLine("That is negative");
+                }
+                if(number == 0)
                 {
                     break;
                 }
-
 
             }
         }
