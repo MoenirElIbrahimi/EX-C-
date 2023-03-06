a3bf7fdb427bee03baadf5d09470b207b80ee8a8
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 13:00:56 +0100

    1-24 EX

--- Program.cs
+++ Program.cs
@@ -4,23 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give the first number!");
+            Console.WriteLine("Your speed!");
             string userInput = Console.ReadLine();
-            double first = Convert.ToDouble(userInput); // ask for 1st int ask for input and convert
-
-            Console.WriteLine("Give the second number!");
-            string userIn = Console.ReadLine();
-            double second = Convert.ToDouble(userIn);// ask for 2nd int ask for input and convert
-
-
-            double plus = first + second;
-            double minus = first - second;
-            double keer = first * second;
-            double delen = first / second;
-            Console.WriteLine(first + " + " + second + " = " + plus + "\n"+
-                first + " - " + second + " = " + minus + "\n"+
-                first + " * " + second + " = " + keer+"\n" +
-                first + " / " + second + " = " + delen); 
+            int speed = Convert.ToInt32(userInput);
+            if (speed > 120) {
+                Console.WriteLine("Speeding!");
+            }
 
 
         }
