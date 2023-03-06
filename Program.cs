e0e03747687d4e944375610be5c9c28c13781d60
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 12:24:03 +0100

    1-16 EX

--- Program.cs
+++ Program.cs
@@ -4,26 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give a string:");
-            String str = Console.ReadLine(); // ask for string
-
+            Console.WriteLine("How many days");
+            int sec = 86400;
             Console.WriteLine("Give an integer:");
-            string userInput = Console.ReadLine();            
-            int intValue = Convert.ToInt32(userInput); // ask for int and convert it
-
-            Console.WriteLine("Give a double:");
-            String usInput = Console.ReadLine();
-            double doubleValue = Convert.ToDouble(usInput); // ask for double and convert it
-            
-            Console.WriteLine("Give a boolean:");
-            string truth = Console.ReadLine();
-            bool booleanValue = System.Convert.ToBoolean(truth);
-            Console.WriteLine(booleanValue); // ask for boolean and convert it
-
-            Console.WriteLine("Your string: " + str + 
-                "\nYour intereger: "  + intValue +
-                "\nYour double: "+ doubleValue +
-                "\nYour boolean: "+ booleanValue);
+            string userInput = Console.ReadLine();
+            int days = Convert.ToInt32(userInput);
+            int sum = days * sec;
+            Console.WriteLine(sum);
 
 
 
