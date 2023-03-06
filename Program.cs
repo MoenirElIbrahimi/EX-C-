e437beae7f5704833c3782a1aed753a3fb54a5f6
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-06 12:44:28 +0100

    1-22 EX

--- Program.cs
+++ Program.cs
@@ -6,15 +6,18 @@
         {
             Console.WriteLine("Give the first number!");
             string userInput = Console.ReadLine();
-            int first = Convert.ToInt32(userInput); // ask for 1st int ask for input and convert
+            double first = Convert.ToDouble(userInput); // ask for 1st int ask for input and convert
 
             Console.WriteLine("Give the second number!");
             string userIn = Console.ReadLine();
-            int second = Convert.ToInt32(userIn);// ask for 2nd int ask for input and convert
+            double second = Convert.ToDouble(userIn);// ask for 2nd int ask for input and convert
 
+            Console.WriteLine("Give the second number!");
+            string user = Console.ReadLine();
+            double third = Convert.ToDouble(user);// ask for 3nd int ask for input and convert
 
-            double sum = first + second;
-            double sumsum= sum / 2;
+            double sum = first + second + third;
+            double sumsum= sum / 3;
             Console.WriteLine("The average is "+ sumsum); 
 
 
