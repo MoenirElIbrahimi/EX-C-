83e716a281c3fa9678d3b8b1b4f4436f8006efe0
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-08 10:37:48 +0100

    2-5 EX

--- Program.cs
+++ Program.cs
@@ -7,27 +7,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            int counter = 0;
+            int end = 0;
             //string srtInput = Console.ReadLine();
-
-           int sumsum = 0;
-            while (true)
-            {
             Console.WriteLine("Give a number:");
             string userInput = Console.ReadLine();
             int number = Convert.ToInt32(userInput);// ask 1st for input and convert it
+
+            for (int i = number; i >= end; end++)
+            {
+                Console.WriteLine(end);
  
                 
-                if(number == 0)
-                {
-                    Console.WriteLine("Total sum of numbers: " + sumsum );
-                    Console.WriteLine("Total amount of numbers: " + counter);
-                    break;
-                }
 
-
-                    sumsum = sumsum + number;
-                    counter++;
                 
                 
                     
