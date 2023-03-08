5a302ba0936dc1044203a2858523dfc77a3f8006
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-08 10:40:22 +0100

    2-8 EX

--- Program.cs
+++ Program.cs
@@ -9,17 +9,20 @@ namespace HelloWorld
 
             int end = 0;
             //string srtInput = Console.ReadLine();
+
+
+            while (true)
+            {
+ 
+
             Console.WriteLine("Give a number:");
             string userInput = Console.ReadLine();
             int number = Convert.ToInt32(userInput);// ask 1st for input and convert it
 
-            for (int i = number; i >= end; end++)
-            {
-                Console.WriteLine(end);
- 
-                
-
-                
+                if (number <= 0) {
+                    Console.WriteLine("Thnx! Bye!");
+                    break;
+                }
                 
                     
               
