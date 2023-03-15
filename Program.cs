d89307d10edb4970d5b7ce2eda3270fe6e76014c
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-15 10:26:58 +0100

    3-24 EX

--- Program.cs
+++ Program.cs
@@ -6,65 +6,36 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //PrintUntilNumber(6);
-            //PrintPhrase();
-            //PrintFromNumberToOne(10);
-            //Division(5,2);
-            DivisibleByThreeInRange(1, 34);
+            Login();
 
 
 
             }
-        public static void PrintPhrase() 
+        public static void Login() 
         {
-            int counter = 0;
-            Console.WriteLine("How many times?");
+
+            Console.WriteLine("Enter username:");
             string userInput = Console.ReadLine();
-            int number = Convert.ToInt32(userInput);
+            Console.WriteLine("Enter password:");
+            string userInput1 = Console.ReadLine();
+            if (userInput == "alex" & userInput1 == "sunshine")
+            {
+                Console.WriteLine("You have successfully logged in!");
+            }
+            else if  (userInput == "emma" & userInput1 == "haskell")
+            {
+                Console.WriteLine("You have successfully logged in!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password!");
+            }
+            //int number = Convert.ToInt32(userInput);
 
-            while (counter < number)
-            {
-                Console.WriteLine("In a hole in the ground there lived a method");
-                counter++;
-            }
-            
-        
-        }
-        public static void PrintUntilNumber(int number)
-        {
-            int counter = 1;
-            while (number >= counter)
-            {
-                Console.WriteLine(counter);
-                counter++;
-            }
-        }
-        public static void PrintFromNumberToOne(int number)
-        {
-            int counter = 1;
-            while (number >= counter)
-            {
-                Console.WriteLine(number);
-                number--;
-            }
-        }
-        public static void Division(int numerator, int denominator) 
-        {
-            //double random = numerator;
-            double result = (double)numerator / denominator;
-            Console.WriteLine(result);
+
+
         }
 
-        public static void DivisibleByThreeInRange(int beginning, int end)
-        {
-            for (int i = beginning; i < end; i++)
-            {
-            if (i % 3 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-        }
-            }
 
 
 
