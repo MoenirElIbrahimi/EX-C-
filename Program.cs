594b0049ba11974759174e70bad3a08a133cd54a
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-27 10:17:56 +0200

    4- 22 EX

--- Program.cs
+++ Program.cs
@@ -2,46 +2,26 @@
 using System.Diagnostics.Metrics;
 using System.Xml.Linq;
 
-List<Book> books = new List<Book>();
-
-while (true)
+internal class Program
 {
-    Console.Write("Name: ");
-    string title = Console.ReadLine().Trim();
-    if (string.IsNullOrEmpty(title))
+    private static void Main(string[] args)
     {
-        break;
+
+        int counter = 0;
+        while (true)
+        {
+            
+            string loop = Console.ReadLine();
+            if (loop == "end")
+            {
+                break;
+            }
+
+
+            counter++;
+
+
+        }
+        Console.WriteLine(counter);
     }
-
-    Console.Write("Pages: ");
-    int pages = int.Parse(Console.ReadLine().Trim());
-
-    Console.Write("Publication year: ");
-    int publicationYear = int.Parse(Console.ReadLine().Trim());
-
-    Book book = new Book(title, pages, publicationYear);
-    books.Add(book);
-}
-
-Console.Write("What information will be printed? ");
-string option = Console.ReadLine().Trim().ToLower();
-
-switch (option)
-{
-    case "everything":
-        foreach (Book book in books)
-        {
-            Console.WriteLine(book);
-        }
-        break;
-
-    case "title":
-        foreach (Book book in books)
-        {
-            Console.WriteLine(book.title);
-        }
-        break;
-
-    default:
-        break;
 }
