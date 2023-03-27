14ac82198254379eff93b0cd5aa10bb59828e4f8
Moenir <1186234@student.roc-nijmegen.nl>
2023-03-27 10:46:18 +0200

    4-23 EX

--- Program.cs
+++ Program.cs
@@ -6,22 +6,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
+        int total = 0;
+        int sum = 0;
+        int number = 0; 
         int counter = 0;
         while (true)
         {
             
             string loop = Console.ReadLine();
+
             if (loop == "end")
             {
                 break;
             }
+            int getal = Convert.ToInt32(loop);
 
+                sum = getal * getal * getal;
+                Console.WriteLine(sum);
 
             counter++;
-
-
         }
-        Console.WriteLine(counter);
+
     }
 }
