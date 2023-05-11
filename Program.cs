f414b984ca3d2bc676ff2ed9ba049b4ef8422ada
Moenir <1186234@student.roc-nijmegen.nl>
2023-05-11 11:53:39 +0200

    EX 5-1

--- Program.cs
+++ Program.cs
@@ -8,25 +8,30 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        string[] guestList = File.ReadAllLines("C:\\Users\\moeni\\OneDrive\\Bureaublad\\school\\C#\\HelloWorld\\guestlist.txt");
-
-        Console.WriteLine("Enter names, an empty line quits.");
+        // Create new timer
+        HelloWorld.Timer timer = new HelloWorld.Timer(100);
+        // Loop until you cancel the loop.
+        // You can cancel with the CTRL + C
         while (true)
         {
-            string name = Console.ReadLine();
-            if (name == "")
+            Console.WriteLine(timer);
+            timer.Advance();
+            // Some error proving, we'll talk about this later.
+            // Known as try-catch.
+            try
             {
-                break;
+                // Wait 100th of a second. 
+                // Sleep(1000) waits one second, if you want to test at slower pace.
+                System.Threading.Thread.Sleep(10);
+            }
+            // Other half of try-catch pair. 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error happened: +" + e);
             }
 
-            if (Array.IndexOf(guestList, name) >= 0)
-            {
-                Console.WriteLine("The name is on the list.");
-            }
-            else
-            {
-                Console.WriteLine("The name is not on the list.");
-            }
+
         }
     }
+    
 }
---
+++ Timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Timer
+    {
+        public int value { get; set; }
+        public int limit { get; set; }
+        public int sec { get; set; }
+
+        public Timer(int limit)
+        {
+            this.limit = limit;
+            this.value = 0;
+        }
+
+        public void Advance()
+        {
+            this.value = this.value + 1;
+            if (this.value >= this.limit)
+            {
+                this.value = 0;
+                this.sec++;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            if (this.value < 10)
+            {
+                return this.sec + ":" + "0" + this.value;
+            }
+
+            return this.sec + ":" + this.value;
+        }
+
+    }
+}
